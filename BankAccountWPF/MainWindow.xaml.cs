f890df7d946f9e385a61b56d0c2a0c65b363717c
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-04-22 07:10:06 -0700

    Added Xaml version

---
+++ BankAccountWPF/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using BankAccountCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BankAccountWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateBankAcc_Click(object sender, RoutedEventArgs e)
+        {
+            double balance = Convert.ToDouble(InitialBalanceTxt.Text);
+            Account acc = new Account(balance);
+
+            MessageBox.Show($"Your balance is {acc.GetBalanceDisplay()}");
+        }
+    }
+}
